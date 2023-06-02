3b9138c8b8820b0ad1122c52fc8bb094f1d59792
MANDON Débora <121875978+DeboraMandon@users.noreply.github.com>
2023-06-02 16:32:26 +0200

    Update 5_MiseAJourBDD.py

--- 5_MiseAJourBDD.py
+++ 5_MiseAJourBDD.py
@@ -3,11 +3,14 @@ from selenium import webdriver #Webdriver de Selenium qui permet de contrôler u
 from selenium.webdriver.common.by import By #Permet d'accéder aux différents élements de la page web
 from selenium.webdriver.common.keys import Keys # Importe les clefs pour les touches du clavier
 from webdriver_manager.chrome import ChromeDriverManager #Assure la gestion du webdriver de Chrome
+from selenium.webdriver.chrome.options import Options
 from time import sleep 
 import time
-import configparser
-import pandas as pd
+import getpass
 import glob
+import pandas as pd
+import os
+import configparser
 import numpy as np
 import re
 
@@ -24,8 +27,10 @@ def scroll(value): #Définition d'une fonction pour scroller automatiquement
         driver.execute_script(f"window.scrollBy(0, {value})")
         time.sleep(0.1) #Temps entre chaque scroll
 
-        
-driver = webdriver.Chrome(ChromeDriverManager().install()) # Ouvrir une page Google Chrome
+chrome_options = Options()
+chrome_options.add_argument("--headless=new")
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options) # Ouvrir une page Google Chrome
+
 time.sleep(2) 
 driver.get('https://www.planning-imadis.fr/planning_gestion.php?d=1&t=1548025200') # Ouvrir l'adresse du site web
 try:
@@ -47,6 +52,7 @@ try:
 except:
     None
 time.sleep(2)    
+
 admin=driver.find_element(By.XPATH, "//*[@id='side-menu']/li[7]/a")
 admin.click()
 time.sleep(2)
@@ -54,55 +60,106 @@ tdg=driver.find_element(By.XPATH, "//*[@id='side-menu']/li[7]/ul/li[3]/a")
 tdg.click()
 scroll(100)
 
+username = getpass.getuser()
 #pour telecharger tous les excel du dernier TDG
+
+#PDS
 excel=driver.find_element(By.CSS_SELECTOR, '#top > div > table > tbody > tr:nth-child(3) > td:nth-child(7) > button.excel.btn.btn-default.btn-outline.btn-sm')
 excel.click()
 time.sleep(1) 
+
 pds=driver.find_element(By.XPATH, "//*[@id='excelDpt1']")
 pds.click()
 time.sleep(1)
+
 generate=driver.find_element(By.ID, "generate")
 generate.click()
-time.sleep(1) 
+time.sleep(2) 
+
+download_folder = 'C:/Users/'+username+'/Downloads/'
+excel_files = glob.glob(download_folder + 'export*.xlsx')
+df_pds=pd.read_excel(excel_files[0], header=None)
+df_pds.to_csv('df_pds.csv', index=False)
+
+os.remove(excel_files[0])
+
+#AO
 ao=driver.find_element(By.XPATH, "//*[@id='excelDpt2']")
 ao.click()
 time.sleep(1)
+
 generate=driver.find_element(By.ID, "generate")
 generate.click()
-time.sleep(1) 
+time.sleep(2) 
+
+download_folder = 'C:/Users/'+username+'/Downloads/'
+excel_files = glob.glob(download_folder + 'export*.xlsx')
+df_ao=pd.read_excel(excel_files[0], header=None)
+df_ao.to_csv('df_ao.csv', index=False)
+
+os.remove(excel_files[0])
+
+#CDS
 cds=driver.find_element(By.XPATH, "//*[@id='excelDpt3']")
 cds.click()
 time.sleep(1)
+
 generate=driver.find_element(By.ID, "generate")
 generate.click()
-time.sleep(1) 
+time.sleep(2) 
+
+download_folder = 'C:/Users/'+username+'/Downloads/'
+excel_files = glob.glob(download_folder + 'export*.xlsx')
+df_cds=pd.read_excel(excel_files[0], header=None)
+df_cds.to_csv('df_cds.csv', index=False)
+
+os.remove(excel_files[0])
+
+#RRU
 rru=driver.find_element(By.XPATH, "//*[@id='excelDpt4']")
 rru.click()
 time.sleep(1)
+
 generate=driver.find_element(By.ID, "generate")
 generate.click()
-time.sleep(1) 
+time.sleep(2) 
+
+download_folder = 'C:/Users/'+username+'/Downloads/'
+excel_files = glob.glob(download_folder + 'export*.xlsx')
+df_rru=pd.read_excel(excel_files[0], header=None)
+df_rru.to_csv('df_rru.csv', index=False)
+
+os.remove(excel_files[0])
+
+#ART
 art=driver.find_element(By.XPATH, "//*[@id='excelDpt6']")
 art.click()
 time.sleep(1) 
+
 generate=driver.find_element(By.ID, "generate")
 generate.click()
-time.sleep(1) 
+time.sleep(2) 
 
+download_folder = 'C:/Users/'+username+'/Downloads/'
+excel_files = glob.glob(download_folder + 'export*.xlsx')
+df_art=pd.read_excel(excel_files[0], header=None)
+df_art.to_csv('df_art.csv', index=False)
+
+os.remove(excel_files[0])
+
+#Fermer le générateur
 close=driver.find_element(By.XPATH, "//*[@id='excel']/div/div/div[3]/button[1]")
 close.click()
 time.sleep(1) 
 
 driver.close()
 
-download_folder = 'C:/Users/d.mandon/Downloads/' # A modifier selon le PC
-excel_files = glob.glob(download_folder + 'export*.xlsx')
 
-df1=pd.read_excel(excel_files[0], header=None)
-df2=pd.read_excel(excel_files[1], header=None)
-df3=pd.read_excel(excel_files[2], header=None)
-df4=pd.read_excel(excel_files[3], header=None)
-df5=pd.read_excel(excel_files[4], header=None)
+df1=pd.read_csv('df_ao.csv')
+df2=pd.read_csv('df_cds.csv')
+df3=pd.read_csv('df_rru.csv')
+df4=pd.read_csv('df_art.csv')
+df5=pd.read_csv('df_pds.csv')
 
 correspondance_colonnes = {0 : 'Date',
                           1: 'Associé',
@@ -148,23 +205,29 @@ df3['Source'] = 'RRU'
 df4['Source'] = 'ART'
 df5['Source'] = 'PDS'
 
-df1['Date'] = df1['Date'].fillna(method='ffill')
-df2['Date'] = df2['Date'].fillna(method='ffill')
-df3['Date'] = df3['Date'].fillna(method='ffill')
-df4['Date'] = df4['Date'].fillna(method='ffill')
-df5['Date'] = df5['Date'].fillna(method='ffill')
-
 df1['Nom_Prenom'] = df1['Nom']+' '+df1['Prénom']
 df2['Nom_Prenom'] = df2['Nom']+' '+df2['Prénom']
 df3['Nom_Prenom'] = df3['Nom']+' '+df3['Prénom']
 df4['Nom_Prenom'] = df4['Nom']+' '+df4['Prénom']
 df5['Nom_Prenom'] = df5['Nom']+' '+df5['Prénom']
 
-df1=df1.drop(['Nom', 'Prénom', 'Jour/Nuit'], axis=1)
-df2=df2.drop(['Nom', 'Prénom', 'Jour/Nuit'], axis=1)
-df3=df3.drop(['Nom', 'Prénom', 'Jour/Nuit'], axis=1)
-df4=df4.drop(['Nom', 'Prénom', 'Jour/Nuit'], axis=1)
-df5=df5.drop(['Nom', 'Prénom', 'Jour/Nuit'], axis=1)
+df1 = df1.dropna(subset=['Nom_Prenom'])
+df2 = df2.dropna(subset=['Nom_Prenom'])
+df3 = df3.dropna(subset=['Nom_Prenom'])
+df4 = df4.dropna(subset=['Nom_Prenom'])
+df5 = df5.dropna(subset=['Nom_Prenom'])
+
+df1['Date'] = df1['Date'].fillna(method='ffill')
+df2['Date'] = df2['Date'].fillna(method='ffill')
+df3['Date'] = df3['Date'].fillna(method='ffill')
+df4['Date'] = df4['Date'].fillna(method='ffill')
+df5['Date'] = df5['Date'].fillna(method='ffill')
+
+df1=df1.drop(['Nom', 'Prénom'], axis=1)
+df2=df2.drop(['Nom', 'Prénom'], axis=1)
+df3=df3.drop(['Nom', 'Prénom'], axis=1)
+df4=df4.drop(['Nom', 'Prénom'], axis=1)
+df5=df5.drop(['Nom', 'Prénom'], axis=1)
 
 df1['Date'] = pd.to_datetime(df1['Date'], format='%d/%m/%Y')
 df2['Date'] = pd.to_datetime(df2['Date'], format='%d/%m/%Y')
@@ -202,12 +265,6 @@ df3['Horaire'] = df3['Heure_Début']+' - '+df3['Heure_Fin']
 df4['Horaire'] = df4['Heure_Début']+' - '+df4['Heure_Fin']
 df5['Horaire'] = df5['Heure_Début']+' - '+df5['Heure_Fin']
 
-df1 = df1.dropna(subset=['Nom_Prenom'])
-df2 = df2.dropna(subset=['Nom_Prenom'])
-df3 = df3.dropna(subset=['Nom_Prenom'])
-df4 = df4.dropna(subset=['Nom_Prenom'])
-df5 = df5.dropna(subset=['Nom_Prenom'])
-
 dfs = [df1, df2, df3, df4, df5]
 
 # Concaténez les DataFrames de la liste en utilisant la fonction concat()
@@ -219,10 +276,10 @@ concatenated_df.reset_index(drop=True, inplace=True)
 concatenated_df['Date'] = pd.to_datetime(concatenated_df['Date'])
 concatenated_df['Date'] = concatenated_df['Date'].dt.strftime("%d/%m/%Y")
 
-concatenated_df.to_csv('C:/Users/d.mandon/Documents/GitHub/PoleMed_BDD/new_data.csv', index=False)
+concatenated_df.to_csv('C:/Users/'+username+'/Imadis Téléradiologie/INTRANET - IMADIS/QUALITE/7- RHM/15 - DMA/GitHub/PoleMed_BDD/new_data.csv', index=False)
 
-df1=pd.read_csv('C:/Users/d.mandon/Documents/GitHub/PoleMed_BDD/Concat_df.csv')
-df2=pd.read_csv('C:/Users/d.mandon/Documents/GitHub/PoleMed_BDD/new_data.csv')
+df1=pd.read_csv('C:/Users/'+username+'/Imadis Téléradiologie/INTRANET - IMADIS/QUALITE/7- RHM/15 - DMA/GitHub/PoleMed_BDD/Concat_df.csv')
+df2=pd.read_csv('C:/Users/'+username+'/Imadis Téléradiologie/INTRANET - IMADIS/QUALITE/7- RHM/15 - DMA/GitHub/PoleMed_BDD/new_data.csv')
 
 dfs = [df1, df2]
 
@@ -241,6 +298,8 @@ data['Date_Heure_Début'] = pd.to_datetime(data['Date_Heure_Début'])
 
 data['Durée'] = data['Date_Heure_Fin'] - data['Date_Heure_Début']
 
+data=data.drop_duplicates()
+
 data['Durée']=data['Durée'].astype(str)
 
 data['Durée']=data['Durée'].str.split().str[2]
@@ -253,4 +312,4 @@ data['Durée'] = pd.to_timedelta(data['Durée'])
 # Calculez la durée en heures décimales
 data['Durée'] = data['Durée'].dt.total_seconds() / 3600
 
-data.to_csv('C:/Users/d.mandon/Documents/GitHub/PoleMed_BDD/BDD.csv', index=False)
+data.to_csv('C:/Users/'+username+'/Imadis Téléradiologie/INTRANET - IMADIS/QUALITE/7- RHM/15 - DMA/GitHub/PoleMed_BDD/BDD.csv', index=False)
