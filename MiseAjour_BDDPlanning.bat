202bb33a18413e0606ac50993bdab12424f49e68
MANDON Débora <121875978+DeboraMandon@users.noreply.github.com>
2023-06-12 14:51:05 +0200

    Update MiseAjour_BDDPlanning.bat

--- MiseAjour_BDDPlanning.bat
+++ MiseAjour_BDDPlanning.bat
@@ -1,5 +1,5 @@
 @echo off
-cd C:\Users\d.mandon\Imadis Téléradiologie\INTRANET - IMADIS\QUALITE\7- RHM\15 - DMA\GitHub\PoleMed_BDD\5_MiseAJourBDD.py
+cd C:\Users\d.mandon\Imadis Téléradiologie\INTRANET - IMADIS\QUALITE\7- RHM\15 - DMA\GitHub\PoleMed_BDD\_MiseAJourBDD.py
 python pip install -r requirements.txt
-python 5_MiseAJourBDD.py
+python _MiseAJourBDD.py
 pause
